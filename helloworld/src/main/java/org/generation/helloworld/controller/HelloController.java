a87e8a1fea61a7aed9e2d6df3530e89dc79540ad
unknown <contact.guttler@gmail.com>
2022-04-27 17:20:37 -0300

    primeiro teste

---
+++ helloworld/src/main/java/org/generation/helloworld/controller/HelloController.java
@@ -0,0 +1,15 @@
+package org.generation.helloworld.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/helloworld")
+public class HelloController {
+
+	@GetMapping
+	public String Hello() {
+		return "Salve!";
+	}
+}
