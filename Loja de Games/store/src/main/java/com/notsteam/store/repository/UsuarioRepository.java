c569e64130bef327cdff3512b4075372204620d7
unknown <contact.guttler@gmail.com>
2022-05-11 14:04:21 -0300

    repository

--- Loja de Games/store/src/main/java/com/notsteam/store/repository/UsuarioRepository.java
+++ Loja de Games/store/src/main/java/com/notsteam/store/repository/UsuarioRepository.java
@@ -2,8 +2,10 @@ package com.notsteam.store.repository;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import com.notsteam.store.model.Usuario;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
 	public Optional<Usuario> findByUsuario(String usuario);
